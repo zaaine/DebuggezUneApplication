2bb32f87e64002d4772b69d1dde627a99d0adfef
zaaine <67982185+zaaine@users.noreply.github.com>
2024-06-18 18:32:00 +0200

    scénario 2 : validé

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -5,7 +5,7 @@ import { getMonth } from "../../helpers/Date";
 import "./style.scss";
 
 const Slider = () => {
-  const { data } = useData();
+  const { data } = useData();  
   const [index, setIndex] = useState(0);
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
     new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
