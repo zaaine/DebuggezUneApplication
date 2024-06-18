b494a1c8af8825bd03462f4a49f79f5f863d27c1
zaaine <67982185+zaaine@users.noreply.github.com>
2024-06-18 19:12:30 +0200

    Scénario 3 : Pendant que je navigue sur le menu, les ancres me permette d'aller directement dans la section concerné.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -22,7 +22,8 @@ const Page = () => {
       <section className="SliderContainer">
         <Slider />
       </section>
-      <section className="ServicesContainer">
+      {/* ajout ancres */}
+      <section id="nos-services" className="ServicesContainer">
         <h2 className="Title">Nos services</h2>
         <p>Nous organisons des événements sur mesure partout dans le monde</p>
         <div className="ListContainer">
@@ -51,11 +52,11 @@ const Page = () => {
           </ServiceCard>
         </div>
       </section>
-      <section className="EventsContainer">
+      <section id="nos-realisations" className="EventsContainer">
         <h2 className="Title">Nos réalisations</h2>
         <EventList />
       </section>
-      <section className="PeoplesContainer">
+      <section id="notre-equipe" className="PeoplesContainer">
         <h2 className="Title">Notre équipe</h2>
         <p>Une équipe d’experts dédiés à l’ogranisation de vos événements</p>
         <div className="ListContainer">
